5a407c20c9777fd2fc616020792ca274c18403b6
Nataly K <doublejanus4@gmail.com>
2022-08-06 11:11:49 +0300

    Ass4

---
+++ Assignment4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while(count < number)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
