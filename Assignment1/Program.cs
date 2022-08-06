ba845ed464f39ac3c53540219f441e213c163108
Nataly K <doublejanus4@gmail.com>
2022-08-06 10:11:34 +0300

    Assignment1

---
+++ Assignment1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine(numberA + " больше " + numberB);
+}
+else if (numberA == numberB)
+{
+    Console.WriteLine(numberA + " = " + numberB);
+}
+else
+{
+    Console.WriteLine(numberB + " больше " + numberA);
+}
