b8b94a56e308ff51660dc6e1632079343b40a842
Nataly K <doublejanus4@gmail.com>
2022-08-06 10:39:18 +0300

    Assignment2

---
+++ Assignment2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(numberB > max)
+{
+    max = numberB;
+    if(numberC > max)
+    {
+        max = numberC;
+    }
+}
+if(numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine(max);
